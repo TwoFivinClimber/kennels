fcdfbdc97a87ef9dd7d056faaad85e86e164cbc5
Brett Hughes <twofivinclimber@gmail.com>
2022-10-23 21:37:06 -0500

    lesson 12

--- request_handler.py
+++ request_handler.py
@@ -80,6 +80,7 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
     def do_GET(self):
+        """handels READ requests to the server"""
         self._set_headers(200)
 
         response = {}
@@ -174,8 +175,6 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any PUT request.
 
     def do_PUT(self):
-        """Handles PUT requests"""
-        self._set_headers(204)
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
         post_body = json.loads(post_body)
@@ -183,20 +182,17 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Parse the URL
         (resource, id) = self.parse_url(self.path)
 
-        # Delete a single animal from the list
-        if resource == "animals":
-            update_animal(id, post_body)
-            
-        if resource == "locations":
-            update_location(id, post_body)
-            
-        if resource == "customers":
-            update_customer(id, post_body)
-            
-        if resource == "employees":
-            update_employee(id, post_body)
+        success = False
 
-    # Encode the new animal and send in response
+        if resource == "animals":
+            success = update_animal(id, post_body)
+        # rest of the elif's
+
+        if success:
+            self._set_headers(204)
+        else:
+            self._set_headers(404)
+
         self.wfile.write("".encode())
         
         
